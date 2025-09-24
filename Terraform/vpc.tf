ba8651dc8796ccf0aaadfb2c6caf247a8b5c9a2b
Corvus <msmark.451@gmail.com>
2025-09-24 19:40:12 +0200

    fixed route table associations

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -113,7 +113,12 @@ resource "aws_route_table" "this" {
   }
 }
 
-resource "aws_route_table_association" "this" {
+resource "aws_route_table_association" "public" {
   subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.this.id
+}
+
+resource "aws_route_table_association" "public-a" {
+  subnet_id = aws_subnet.public-a.id
   route_table_id = aws_route_table.this.id
 }

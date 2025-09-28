59a9bec0858f2e89a1fe876f899ae6bdd14bbd08
Corvus451 <msmark.451@gmail.com>
2025-09-28 18:18:35 +0200

    updated outputs

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -11,3 +11,19 @@
 #   description = "The URL to access the pods via the ALB"
 #   value       = try("http://${kubernetes_ingress_v1.ingress.status[0].load_balancer[0].ingress[0].hostname}", "Waiting for ALB to be provisioned")
 # }
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "db_address" {
+  value = module.database.database_address
+}
+
+output "db_name" {
+  value = module.database.db_name
+}
+
+output "bastion_ip" {
+  value = module.bastion.bastion_ip
+}

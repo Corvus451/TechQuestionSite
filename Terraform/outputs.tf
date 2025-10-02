85803f3e7ce55e418acf7a700e0cd4089f641379
Corvus451 <msmark.451@gmail.com>
2025-10-02 22:45:43 +0200

    removed redundant lines from terraform outputs

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,17 +1,3 @@
-# output "cluster_endpoint" {
-#   value = aws_eks_cluster.this.endpoint
-# }
-
-# output "database_endpoint" {
-#   value = aws_db_instance.this.endpoint
-# }
-
-# Output the Ingress address (ALB URL)
-# output "ingress_address" {
-#   description = "The URL to access the pods via the ALB"
-#   value       = try("http://${kubernetes_ingress_v1.ingress.status[0].load_balancer[0].ingress[0].hostname}", "Waiting for ALB to be provisioned")
-# }
-
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }

23784d533351a64d1418d86f1bc67ac2ed74c39a
Corvus451 <msmark.451@gmail.com>
2025-10-09 14:08:39 +0200

    created init_auth.sql to separate auth database

--- Database/init.sql
+++ Database/init.sql
@@ -5,8 +5,8 @@ DROP TABLE IF EXISTS answers;
 CREATE TABLE users (
 user_id serial PRIMARY KEY,
 username varchar(15) NOT NULL UNIQUE,
-password varchar(255) NOT NULL,
-admin boolean DEFAULT false
+admin boolean DEFAULT false,
+created_at timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE questions (

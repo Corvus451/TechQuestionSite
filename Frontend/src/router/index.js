cb012f9e9e597f5985c61ddb0b3baf83df57dd19
Corvus451 <msmark.451@gmail.com>
2026-03-23 22:30:32 +0100

    Frontend add route authorization

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -3,6 +3,7 @@ import HomePage from "../pages/HomePage.vue";
 import RegisterPage from "../pages/RegisterPage.vue";
 import LoginPage from "../pages/LoginPage.vue";
 import CreateQuestion from "../pages/CreateQuestion.vue";
+import { isLoggedIn } from "../utility/status";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,19 +16,33 @@ const router = createRouter({
         {
             path: '/register',
             name: 'register',
-            component: RegisterPage
+            component: RegisterPage,
+            meta: {noAuth: true}
         },
         {
             path: '/login',
             name: 'login',
-            component: LoginPage
+            component: LoginPage,
+            meta: {noAuth: true}
         },
         {
             path: '/postquestion',
             name: 'postquestion',
-            component: CreateQuestion
+            component: CreateQuestion,
+            meta: {authRequired: true}
         }
     ]
+});
+
+router.beforeEach((to, from) => {
+    if (to.meta.authRequired && !isLoggedIn()) {
+        return '/login';
+    }
+
+    console.log("logged in:", isLoggedIn());
+    if (to.meta.noAuth && isLoggedIn()) {
+        return '/';
+    };
 });
 
 export default router
